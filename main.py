d1ccb7e0d82e7f95ef2cbcff1b9907b6ed1d801f
dytrgn <tariganadytta41@gmail.com>
2024-09-21 15:15:18 +0700

    Latihan Pelcod 21 September 2024

---
+++ main.py
@@ -0,0 +1,39 @@
+#nilai = int(input("masukkan nilai: "))
+
+'''
+if (nilai > 90 and nilai  <= 100):
+    print("selamat anda lulus")
+elif (nilai > 80):
+    print("selamat anda lulus")
+elif (nilai > 70):
+    print("hampir lulus kauu")
+else:
+    print("anda masih belum lulus")
+'''
+
+#looping
+#for loop
+#for i in range(10, 0, -2):
+#    print(1)
+#    print("hello world")
+
+#angka = int(input("masukkan angka"))
+
+#while (angka > 1):
+#    print("1")
+#    angka -= 1
+
+#function
+#def
+'''
+def sayHello():
+    print("hello world")
+
+sayHello()
+'''
+
+fungsi_kurang4 = lambda param_angka : param_angka * 4
+
+angka = int(input("masukkan angka"))
+
+print(fungsi_kurang4(angka))
